3fb876579ad11045ff03a53d2c247b507e99cea9
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-02-11 14:37:40 +0000

    Add an internal Exceptions base class

--- lib/cyclid/errors.rb
+++ lib/cyclid/errors.rb
@@ -15,6 +15,16 @@ module Cyclid
         AUTH_FAILURE = 3
         # A unique entry already exists
         DUPLICATE = 4
+
+        # User does not exist
+        INVALID_USER = 10
+      end
+    end
+
+    # Internal exceptions
+    module Exceptions
+      # Base class for all internal Cyclid exceptions
+      class CyclidError < StandardError
       end
     end
   end
