a980e204dca3e107cb7bd086f7fb7a5984d95ec6
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-02-15 14:47:09 +0000

    Complete the GET & PUT endpoints for org members
    Finish the /organizations/:name/members/:username GET endpoint.
    Add a PUT endpoint for /organizations/:name/members/:username to update users
    organization permissions.

--- lib/cyclid/controllers/organization.rb
+++ lib/cyclid/controllers/organization.rb
@@ -115,13 +115,22 @@ module Cyclid
         halt_with_json_response(404, INVALID_USER,'user does not exist') \
           if user.nil?
 
-        Cyclid.logger.debug "Got user #{user.username} for organization #{org.name}"
-
         begin
-          Cyclid.logger.debug "#{user.username} perms: #{user.userpermissions.to_a}"
           perms = user.userpermissions.find_by(organization: org)
 
-          Cyclid.logger.debug "Got #{perms} for user"
+          user_hash = user.serializable_hash
+          user_hash.delete_if do |key, _value|
+            key == 'password' || key == 'secret'
+          end
+
+          perms_hash = perms.serializable_hash
+          perms_hash.delete_if do |key, _value|
+            key == 'id' || key == 'user_id' || key == 'organization_id'
+          end
+
+          user_hash['permissions'] = perms_hash
+
+          return user_hash.to_json
         rescue ActiveRecord::ActiveRecordError, \
                ActiveRecord::UnknownAttributeError => ex
 
@@ -129,6 +138,42 @@ module Cyclid
           halt_with_json_response(500, INTERNAL_ERROR, ex.message)
         end
       end
+
+      put '/organizations/:name/members/:username' do
+        authorized_for!(params[:name], Operations::WRITE)
+
+        payload = json_request_body
+        Cyclid.logger.debug payload
+
+        org = Organization.find_by(name: params[:name])
+        halt_with_json_response(404, INVALID_ORG,'organization does not exist') \
+          if org.nil?
+
+        user = org.users.find_by(username: params[:username])
+        halt_with_json_response(404, INVALID_USER,'user does not exist') \
+          if user.nil?
+
+        begin
+          perms = user.userpermissions.find_by(organization: org)
+
+          payload_perms = payload['permissions'] if payload.has_key? 'permissions'
+          unless payload_perms.nil?
+            perms.admin = payload_perms['admin'] if payload_perms.has_key? 'admin'
+            perms.write = payload_perms['write'] if payload_perms.has_key? 'write'
+            perms.read = payload_perms['read'] if payload_perms.has_key? 'read'
+
+            Cyclid.logger.debug perms.serializable_hash
+
+            perms.save!
+          end
+        rescue ActiveRecord::ActiveRecordError, \
+               ActiveRecord::UnknownAttributeError => ex
+
+          Cyclid.logger.debug ex.message
+          halt_with_json_response(500, INTERNAL_ERROR, ex.message)
+        end
+      end
+
     end
 
     # Register this controller
