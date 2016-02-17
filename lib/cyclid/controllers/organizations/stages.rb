1b142605187b6349a98c09c4be13abd623605455
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-02-17 22:04:43 +0000

    Improve the stages endpoints
    Add usable endpoints for GET & POST of stages.
    Fix authorization for those endpoints.

--- lib/cyclid/controllers/organizations/stages.rb
+++ lib/cyclid/controllers/organizations/stages.rb
@@ -7,15 +7,58 @@ module Cyclid
       # API endpoints for Organization Stages
       module Stages
         def self.registered(app)
+          include Errors::HTTPErrors
+
           # @macro [attach] sinatra.get
           #   @overload get "$1"
           # @method get_organizations_organization_stages
           # @return [String] JSON represention of all the stages with the organization.
           # Get all of the stages.
           app.get do
-            authorized_admin!(Operations::READ)
+            authorized_for!(params[:name], Operations::READ)
 
-            return Stage.all.to_json
+            # Retrieve the stage data in a form we can more easily manipulate so
+            # that we can sanitize it
+            stages = Stage.all_as_hash
+
+            # Clean it up
+            stages.map! do |stage|
+              sanitize_stage(stage)
+            end
+
+            return stages.to_json
+          end
+
+          # @macro [attach] sinatra.post
+          #   @overload post "$1"
+          # @method post_organizations_organization_stages
+          # Create a new stage.
+          app.post do
+            authorized_for!(params[:name], Operations::ADMIN)
+
+            payload = json_request_body
+            Cyclid.logger.debug payload
+
+            org = Organization.find_by(name: params[:name])
+            halt_with_json_response(404, INVALID_ORG, 'organization does not exist') \
+              if org.nil?
+
+            begin
+              stage = Stage.new
+
+              stage.name = payload['name']
+              stage.version = payload['version'] if payload.key? 'version'
+              stage.organization = org
+
+              # XXX: Create & seralize the actions
+
+              stage.save!
+            rescue ActiveRecord::ActiveRecordError, \
+                   ActiveRecord::UnknownAttributeError => ex
+
+              Cyclid.logger.debug ex.message
+              halt_with_json_response(400, INVALID_JSON, ex.message)
+            end
           end
         end
       end
--- lib/cyclid/models/stage.rb
+++ lib/cyclid/models/stage.rb
@@ -6,10 +6,18 @@ module Cyclid
     class Stage < ActiveRecord::Base
       Cyclid.logger.debug('In the Stage model')
 
+      class << self
+        # Return the collection of Stages as an array of Hashes (instead
+        # of Stage objects)
+        def all_as_hash
+          all.to_a.map(&:serializable_hash)
+        end
+      end
+
       validates :name, presence: true
       validates :version, presence: true
 
-      validates_uniqueness_of :name
+      validates_uniqueness_of :name, scope: :version
       validates_format_of :version, with: /\A\d+.\d+.\d+.?\d*\z/
 
       belongs_to :organization
