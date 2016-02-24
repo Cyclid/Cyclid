0c66c583b314a5c4db67c704dd3e74ea82cbdf23
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-02-24 15:59:51 +0000

    Start to add Builders & BuildHosts
    Jobs will require hosts to build on; Builders provide BuildHosts, which in
    turn can be connected to Transports to run Actions.
    Add the Builder & BuildHost base classes.
    Add a "Mist" Builder plugin (currently for testing)
    Add an extended "builder" test framework (cloned from the "plugin" test)

---
+++ lib/cyclid/plugins/builder.rb
@@ -0,0 +1,67 @@
+# Top level module for the core Cyclid code.
+module Cyclid
+  # Module for the Cyclid API
+  module API
+    # Module for Cyclid Plugins
+    module Plugins
+      # Base class for BuildHost
+      class BuildHost < Hash
+        def initialize(args)
+          args.each do |key, value|
+            self[key.to_sym] = value
+          end
+        end
+
+        # Return the information needed (hostname/IP, username, password if there
+        # is one) to create a Transport to this host, in a normalized form.
+        def connect_info
+          [self[:hostname], self[:username], self[:password]]
+        end
+
+        # Return a list of acceptable Transports that can be used to connect to this
+        # host.
+        def transports
+          # XXX Maybe create some constants for "well known" Transports such as 'ssh'
+          []
+        end
+
+        # Return free-form data about this host that may be useful to the build
+        # process and can be merged into the Job context. This may be a subset of the
+        # data for this BuildHost, or the full set.
+        def context_info
+          self.dup
+        end
+      end
+
+      # Base class for Builders
+      class Builder < Base
+        # Create a build host, probably on a remote system, and return information
+        # about it in a BuildHost object that encapsulates the information about it.
+        def initialize(*args)
+        end
+
+        # Get or create a build host that can be used by a job. Args will be things
+        # like the OS & version required, taken from the 'environment' section of the
+        # job definition.
+        # 
+        # The Builder can call out to external service E.g. AWS, DO, RAX etc. or
+        # return an existing instance from a pool
+        def get(*args)
+        end
+
+        # XXX Do we want prepare() & destroy() methods on the BuildHost, instead?
+
+        # Prepare the build host for the job, if required E.g. install any extra
+        # packages that are listed in the 'environment' section of the job definition.
+        def prepare(transport, buildhost, env={})
+        end
+
+        # Shut down/release/destroy (if appropriate) the build host
+        def release(transport, buildhost)
+        end
+      end
+    end
+  end
+end
+
+require_rel 'builder/*.rb'
---
+++ lib/cyclid/plugins/builder/mist.rb
@@ -0,0 +1,62 @@
+# Top level module for the core Cyclid code.
+module Cyclid
+  # Module for the Cyclid API
+  module API
+    # Module for Cyclid Plugins
+    module Plugins
+      # Mist build host
+      class MistHost < BuildHost
+        # SSH is the only acceptable Transport
+        def transports
+          ['ssh']
+        end 
+      end
+
+      # Mist builder. Calls out to Mist to obtain a build host instance.
+      class Mist < Builder
+        def initialize(args = {})
+          args.symbolize_keys!
+
+          @os = args[:os]
+        end
+
+        def get(args = {})
+          # XXX Just return a random host from these two, for testing
+          hosts = ['r1','r2']
+          MistHost.new(hostname: hosts.sample, username: 'sys-ops', password: nil, distro: 'ubuntu')
+        end
+ 
+        def prepare(transport, buildhost, env = {})
+          distro = buildhost[:distro]
+
+          if distro == 'ubuntu' || distro == 'debian'
+            env[:repos].each do |repo|
+              if distro == 'ubuntu'
+                transport.exec "sudo add-apt-repository #{repo}"
+              elsif distro == 'debian'
+                # XXX
+              end
+            end
+
+            transport.exec 'sudo apt-get update'
+
+            env[:packages].each do |package|
+              transport.exec "sudo apt-get install -y #{package}"
+            end
+          elsif distro == 'redhat' || distro == 'fedora'
+            env[:packages].each do |package|
+              transport.exec "sudo yum install #{package}"
+            end
+          end
+        end
+
+        def release(transport, buildhost)
+          transport.exec "echo sudo shutdown -h now"
+        end
+
+        # Register this plugin
+        register_plugin 'mist'
+      end
+    end
+  end
+end
