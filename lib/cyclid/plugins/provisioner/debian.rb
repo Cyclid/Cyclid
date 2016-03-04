b61f5560b31af58c7a7031107ed0d1760dc27274
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-03-04 19:35:21 +0000

    The usual Rubocop dance

--- lib/cyclid/plugins/provisioner/debian.rb
+++ lib/cyclid/plugins/provisioner/debian.rb
@@ -8,26 +8,24 @@ module Cyclid
       class Debian < Provisioner
         # Prepare a Debian based build host
         def prepare(transport, buildhost, env = {})
-          begin
-            if env.key? :repos
-              env[:repos].each do |repo|
-                # XXX Repos probably need to be more complex than a simple list;
-                # URLs, components & key ID's will be required
-                raise 'adding repositories on Debian is not yet supported!'
-              end
-
-              success = transport.exec 'sudo apt-get update'
-              raise "failed to update repositories" unless success
+          if env.key? :repos
+            env[:repos].each do |_repo|
+              # XXX Repos probably need to be more complex than a simple list;
+              # URLs, components & key ID's will be required
+              raise 'adding repositories on Debian is not yet supported!'
             end
 
-            env[:packages].each do |package|
-              success = transport.exec "sudo apt-get install -y #{package}"
-              raise "failed to install package #{package}" unless success
-            end if env.key? :packages
-          rescue StandardError => ex
-            Cyclid.logger.error "failed to provision #{buildhost[:name]}: #{ex}"
-            raise
+            success = transport.exec 'sudo apt-get update'
+            raise 'failed to update repositories' unless success
           end
+
+          env[:packages].each do |package|
+            success = transport.exec "sudo apt-get install -y #{package}"
+            raise "failed to install package #{package}" unless success
+          end if env.key? :packages
+        rescue StandardError => ex
+          Cyclid.logger.error "failed to provision #{buildhost[:name]}: #{ex}"
+          raise
         end
 
         # Register this plugin
--- lib/cyclid/plugins/provisioner/ubuntu.rb
+++ lib/cyclid/plugins/provisioner/ubuntu.rb
@@ -8,26 +8,24 @@ module Cyclid
       class Ubuntu < Provisioner
         # Prepare an Ubuntu based build host
         def prepare(transport, buildhost, env = {})
-          begin
-            if env.key? :repos
-              env[:repos].each do |repo|
-                # XXX Check that it's actually a PPA
-                success = transport.exec "sudo apt-add-repository -y #{repo}"
-                raise "failed to add repository #{repo}" unless success
-              end
-
-              success = transport.exec 'sudo apt-get update'
-              raise "failed to update repositories" unless success
+          if env.key? :repos
+            env[:repos].each do |repo|
+              # XXX Check that it's actually a PPA
+              success = transport.exec "sudo apt-add-repository -y #{repo}"
+              raise "failed to add repository #{repo}" unless success
             end
 
-            env[:packages].each do |package|
-              success = transport.exec "sudo apt-get install -y #{package}"
-              raise "failed to install package #{package}" unless success
-            end if env.key? :packages
-          rescue StandardError => ex
-            Cyclid.logger.error "failed to provision #{buildhost[:name]}: #{ex}"
-            raise
+            success = transport.exec 'sudo apt-get update'
+            raise 'failed to update repositories' unless success
           end
+
+          env[:packages].each do |package|
+            success = transport.exec "sudo apt-get install -y #{package}"
+            raise "failed to install package #{package}" unless success
+          end if env.key? :packages
+        rescue StandardError => ex
+          Cyclid.logger.error "failed to provision #{buildhost[:name]}: #{ex}"
+          raise
         end
 
         # Register this plugin
