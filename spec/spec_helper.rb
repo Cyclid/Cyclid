8265264f306ec7f544d7d8edb34dd60a7a94da4d
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-03-12 22:30:38 +0000

    Ignore code we don't care about

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 require 'bundler/setup'
 require 'simplecov'
-SimpleCov.start
+
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/test/'
+  add_filter '/db/'
+end
 
 require_relative '../lib/cyclid'
