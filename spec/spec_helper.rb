074431d3a6cfa91f15b40e0a99c0f3206ce14fa9
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-03-15 20:09:44 +0000

    Create a test database & load the AR schema.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,16 @@ SimpleCov.start do
   add_filter '/db/'
 end
 
+# Configure a test database
+require 'active_record'
+
+ENV['RACK_ENV'] = 'test'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: 'memory'
+)
+require_relative '../db/schema.rb'
+
+# Pull in the code
 require_relative '../lib/cyclid'
