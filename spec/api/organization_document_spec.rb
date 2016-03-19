ebfafb2b4ad445bce79c5f0afd723c83f23eaf5f
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-03-19 11:36:04 +0000

    Add more Organization tests
    Add tests for organization documents.
    Add tests for organization members.

---
+++ spec/api/organization_document_spec.rb
@@ -0,0 +1,119 @@
+require 'spec_helper'
+require 'json'
+
+describe 'an organization document' do
+  include Rack::Test::Methods
+
+  before :all do
+    new_database
+  end
+
+  it 'requires authentication' do
+    get '/organizations/admins'
+    expect(last_response.status).to eq(401)
+  end
+
+  it 'return a valid organization' do
+    authorize 'admin', 'password'
+    get '/organizations/admins'
+    expect(last_response.status).to eq(200)
+
+    res_json = JSON.parse(last_response.body)
+    expect(res_json).to eq({'id'=>1,
+                            'name'=>'admins',
+                            'owner_email'=>'admins@example.com',
+                            'users'=>['admin']})
+  end
+
+  it 'fails if the organizations does not exist' do
+    authorize 'admin', 'password'
+    get '/organizations/test'
+
+    expect(last_response.status).to eq(404)
+  end
+
+  context 'modifying an organization' do
+    it 'changes the owner email address' do
+      modified_org = {'owner_email'=>'test@example.com'}
+
+      authorize 'admin', 'password'
+      put_json '/organizations/admins', modified_org.to_json
+      expect(last_response.status).to eq(200)
+
+      # Retrieve the organization record and check that it changed
+      authorize 'admin', 'password'
+      get '/organizations/admins'
+      expect(last_response.status).to eq(200)
+
+      res_json = JSON.parse(last_response.body)
+
+      expect(res_json).to eq({'id'=>1,
+                              'name'=>'admins',
+                              'owner_email'=>'test@example.com',
+                              'users'=>['admin']})
+    end
+
+    it 'adds a new user' do
+      # Create a test user that isn't in the organization
+      new_user = {'username'=>'test',
+                  'email'=>'test@example.com'}
+
+      authorize 'admin', 'password'
+      post_json '/users', new_user.to_json
+      expect(last_response.status).to eq(200)
+
+      # Add the user to the organization
+      modified_org = {'users'=>['admin','test']}
+
+      authorize 'admin', 'password'
+      put_json '/organizations/admins', modified_org.to_json
+      expect(last_response.status).to eq(200)
+
+      # Retrieve the organization record and check that it changed
+      authorize 'admin', 'password'
+      get '/organizations/admins'
+      expect(last_response.status).to eq(200)
+
+      res_json = JSON.parse(last_response.body)
+
+      expect(res_json).to eq({'id'=>1,
+                              'name'=>'admins',
+                              'owner_email'=>'test@example.com',
+                              'users'=>['admin','test']})
+    end
+
+    it 'removes a user' do
+      modified_org = {'users'=>['admin']}
+
+      authorize 'admin', 'password'
+      put_json '/organizations/admins', modified_org.to_json
+      expect(last_response.status).to eq(200)
+
+      # Retrieve the organization record and check that it changed
+      authorize 'admin', 'password'
+      get '/organizations/admins'
+      expect(last_response.status).to eq(200)
+
+      res_json = JSON.parse(last_response.body)
+
+      expect(res_json).to eq({'id'=>1,
+                              'name'=>'admins',
+                              'owner_email'=>'test@example.com',
+                              'users'=>['admin']})
+    end
+
+    it 'does not add an invalid user' do
+      modified_org = {'users'=>['invalid']}
+
+      authorize 'admin', 'password'
+      put_json '/organizations/admins', modified_org.to_json
+      expect(last_response.status).to eq(404)
+    end
+
+    it 'fails if the new data is invalid' do
+      authorize 'admin', 'password'
+      put_json '/organizations/admins', 'this is not valid JSON' 
+      expect(last_response.status).to eq(400)
+    end
+  end
+end
---
+++ spec/api/organization_members_spec.rb
@@ -0,0 +1,82 @@
+require 'spec_helper'
+require 'json'
+
+describe 'an organization member' do
+  include Rack::Test::Methods
+
+  before :all do
+    new_database
+  end
+
+  context 'retrieving a member' do
+    it 'returns a valid sanitized organization member' do
+      authorize 'admin', 'password'
+      get '/organizations/admins/members/admin'
+      expect(last_response.status).to eq(200)
+
+      # The response _should not_ include the users password, secret, or any foreign keys
+      res_json = JSON.parse(last_response.body)
+      expect(res_json).to eq({'id'=>1,
+                              'username'=>'admin',
+                              'email'=>'admin@example.com',
+                              'permissions'=> {
+                                'admin'=>true,
+                                'write'=>true,
+                                'read'=>true
+                              }})
+    end
+
+    it 'fails to return an invalid organization member' do
+      authorize 'admin', 'password'
+      get '/organizations/admins/members/invalid'
+      expect(last_response.status).to eq(404)
+    end
+  end
+
+  context 'modifying a member' do
+    it 'changes the users read permission' do
+      new_perms = {permissions: {read: false}}
+
+      authorize 'admin', 'password'
+      put_json '/organizations/admins/members/admin', new_perms.to_json
+      expect(last_response.status).to eq(200)
+
+      # Retrieve the memmber and check that it changed
+      authorize 'admin', 'password'
+      get '/organizations/admins/members/admin'
+      expect(last_response.status).to eq(200)
+
+      res_json = JSON.parse(last_response.body)
+      expect(res_json).to eq({'id'=>1,
+                              'username'=>'admin',
+                              'email'=>'admin@example.com',
+                              'permissions'=> {
+                                'admin'=>true,
+                                'write'=>true,
+                                'read'=>false
+                              }})
+    end
+
+    it 'does not fail if new permissions are not given' do
+      new_perms = {}
+
+      authorize 'admin', 'password'
+      put_json '/organizations/admins/members/admin', new_perms.to_json
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'ignores unknown permissions' do
+      new_perms = {permissions: {invalid: true}}
+
+      authorize 'admin', 'password'
+      put_json '/organizations/admins/members/admin', new_perms.to_json
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'fails if the new data is invalid' do
+      authorize 'admin', 'password'
+      put_json '/organizations/admins/members/admin', 'this is not valid JSON' 
+      expect(last_response.status).to eq(400)
+    end
+  end
+end
