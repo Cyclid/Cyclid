cf19e3e9b04d5239d72e792b3a440719a02f50d3
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-07-30 15:30:53 +0100

    Allow PUTS in CORS

--- lib/cyclid/controllers.rb
+++ lib/cyclid/controllers.rb
@@ -53,7 +53,7 @@ module Cyclid
       configure do
         enable :cross_origin
         set :allow_origin, :any
-        set :allow_methods, [:get, :post, :options]
+        set :allow_methods, [:get, :put, :post, :options]
         set :allow_credentials, true
         set :max_age, "1728000"
         set :expose_headers, ['Content-Type']
