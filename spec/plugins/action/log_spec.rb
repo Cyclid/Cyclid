1cbcc9785bc1c9729f8840ed1cebbe4f3c943ced
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-10-26 20:53:33 +0000

    Let Rubocop be Rubocop

--- spec/plugins/action/log_spec.rb
+++ spec/plugins/action/log_spec.rb
@@ -42,10 +42,9 @@ describe Cyclid::API::Plugins::Log do
       expect do
         plugin = Cyclid::API::Plugins::Log.new(message: 'this is a %{data}')
       end.to_not raise_error
-      expect{ plugin.prepare(transport: nil, ctx: {data: 'message'}) }.to_not raise_error
+      expect{ plugin.prepare(transport: nil, ctx: { data: 'message' }) }.to_not raise_error
       expect(plugin.perform(log)).to be true
       expect(log.data).to eq('this is a message')
     end
-
   end
 end
--- spec/spec_stubs.rb
+++ spec/spec_stubs.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # Stub LogBuffer analogue
 class TestLog
   attr_reader :data
